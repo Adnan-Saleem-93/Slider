5898475b6ab2ad51a66e35e21555c715b5d2a5b9
Adnan Saleem <adnan13893@gmail.com>
2021-08-02 12:54:26 +0500

    added button click functions & slider transition

--- src/components/review-section.jsx
+++ src/components/review-section.jsx
@@ -3,15 +3,25 @@ import Reviews from "./reviews";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleDoubleLeft, faAngleDoubleRight} from "@fortawesome/free-solid-svg-icons";
 import {Button, Row, Col} from "react-bootstrap";
+import people from "../utils/data";
 
 const ReviewSection = () => {
   const [activeReview, setActiveReview] = useState(1);
-  const handleButtonClick = () => {};
+  const handleButtonClick = (direction) => {
+    if (direction === "right")
+      setActiveReview(activeReview === people.length ? 1 : activeReview + 1);
+    else setActiveReview(activeReview === 1 ? people.length : activeReview - 1);
+  };
 
   return (
     <Row className="main-section">
       <Col md={1}>
-        <Button className="btn-left" variant="light" title="Previous" onClick={handleButtonClick}>
+        <Button
+          className="btn-left"
+          variant="light"
+          title="Previous"
+          onClick={() => handleButtonClick("left")}
+        >
           <FontAwesomeIcon icon={faAngleDoubleLeft} size="lg" />
         </Button>
       </Col>
@@ -19,7 +29,12 @@ const ReviewSection = () => {
         <Reviews activeReview={activeReview} />
       </Col>
       <Col md={1}>
-        <Button className="btn-right" variant="light" title="Next" onClick={handleButtonClick}>
+        <Button
+          className="btn-right"
+          variant="light"
+          title="Next"
+          onClick={() => handleButtonClick("right")}
+        >
           <FontAwesomeIcon icon={faAngleDoubleRight} size="lg" />
         </Button>
       </Col>
--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -1,15 +1,59 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect} from "react";
 import people from "../utils/data";
 import {Row, Col, Image} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faQuoteRight, faQuoteLeft} from "@fortawesome/free-solid-svg-icons";
 
 const Reviews = ({activeReview}) => {
-  useEffect(() => {}, [activeReview]);
+  let len = people.length;
+  useEffect(() => {
+    // first remove all 'next and prev' slide classes
+    let articles = document.querySelectorAll("[class^=article_]");
+    console.log(articles);
+    articles.forEach((item) => {
+      item.classList.remove("active-slide", "prev-slide", "next-slide");
+    });
+
+    // then, assign 'active' class to active review
+    document.getElementById(`article_${activeReview}`).classList.add("active-slide");
+
+    // next, we assign 'next and prev slide' classes based on activeReview value
+    // if 1st review
+    if (activeReview === 1) {
+      document.getElementById(`article_${len}`).classList.add("prev-slide");
+      for (let i = 2; i < len; i++) {
+        document.getElementById(`article_${i}`).classList.add("next-slide");
+      }
+    }
+    // if last review
+    else if (activeReview === len) {
+      document.getElementById(`article_1`).classList.add("next-slide");
+      for (let i = 2; i < len; i++) {
+        document.getElementById(`article_${i}`).classList.add("prev-slide");
+      }
+    }
+    // if middle review
+    else {
+      // apply prev-slide class to previous elements
+      for (let i = 1; i < activeReview; i++) {
+        document.getElementById(`article_${i}`).classList.add("prev-slide");
+      }
+      // apply next-slide class to next elements
+      for (let i = activeReview + 1; i <= len; i++) {
+        document.getElementById(`article_${i}`).classList.add("next-slide");
+      }
+    }
+  }, [activeReview, len]);
+
   const renderReviewsList = () => {
     return people.map(({id, name, image, title, quote}, index) => {
       return (
-        <section key={index} hidden={activeReview !== id ? true : false} className="text-center">
+        <article
+          key={index}
+          hidden={activeReview !== id ? true : false}
+          className={`article_${id} text-center`}
+          id={`article_${id}`}
+        >
           <Row>
             <Col>
               <Image src={image} alt={name} roundedCircle />
@@ -24,12 +68,12 @@ const Reviews = ({activeReview}) => {
               </p>
             </Col>
           </Row>
-        </section>
+        </article>
       );
     });
   };
 
-  return <article>{renderReviewsList()}</article>;
+  return <section>{renderReviewsList()}</section>;
 };
 
 export default Reviews;
